34fc5541038149bb739cda40da0e60904d8b1690
Michael Tier <mtier@datto.com>
2019-06-02 17:56:48 -0400

    Adds AMI ID specification variable

--- vars.tf
+++ vars.tf
@@ -2,4 +2,9 @@ variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
   default = "us-east-1"
+  description = "AWS Region where module should operate)"
+}
+variable "AMI_ID" {
+  default = "ami-04a3d424ed14a1996"
+  description = "The AMI to use in the Auto Scaling group"
 }
