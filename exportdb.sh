994d0391f491a99847587ee56fe3e11d711e0fa1
Michael Tier <mtier@datto.com>
2019-06-05 20:58:25 -0400

    changes path in export db script to use ec2-user ~

--- exportdb.sh
+++ exportdb.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # clean up old backup
-rm -rf /tmp/backup_db/cache.db
+rm -rf /home/ec2-user/backup_db/cache.db
 # export the database into the temp directory
-sqlite3 cache.db .dump > /tmp/backup_db/cache.db
+sqlite3 cache.db .dump > /home/ec2-user/backup_db/cache.db
 #copy the new backup to the s3 bucket
-aws s3 cp /tmp/backup_db/cache.db s3://mjtier.development.terraform.backup/cache.db
+aws s3 cp /home/ec2-user/backup_db/cache.db s3://mjtier.development.terraform.provisioning/cache.db
--- setup.sh
+++ setup.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
 
+FILE=/home/ec2-user/import_db/cache.db
+
 # warm the db
 mkdir -p /home/ec2-user/import_db
-aws s3 cp  s3://mjtier.development.terraform.backup/cache.db /home/ec2-user/cahce.db
+aws s3 cp  s3://mjtier.development.terraform.provisioning/cache.db $FILE
 
 # if the cahced db from the s3 bucket was copied, this is not the first time
 # that the infastructure was provisioned by terraform
 
-FILE=/tmp/import_db/cache.db
 if [ -f "$FILE" ]; then
     echo "$FILE exist"
     sqlite3 .read $FILE
 fi
+
+
+
 # setup cron job to run every 5 minutes to export the database
-mkdir -p  /tmp/backup_db
+mkdir -p  /home/ec2/backup_db
 # grab the export script from an s3 bucket
 aws s3 cp  s3://mjtier.development.terraform.provisioning/exportdb.sh /home/ec2-user/exportdb.sh
 chmod a+x /home/ec2-user/exportdb.sh
-crontab -e */5 * * * * /tmp/exportdb.sh
+crontab -e */5 * * * * /home/ec2-user/exportdb.sh
