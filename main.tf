3570954bfe699451eef847c238cf59863a71be39
Michael Tier <mtier@datto.com>
2019-06-05 20:56:55 -0400

    Refactors out policy code to main & add json conf

--- main.tf
+++ main.tf
@@ -10,6 +10,27 @@ resource "aws_instance" "sqlite-cache-instance" {
   iam_instance_profile = aws_iam_instance_profile.web_instance_profile.id
 }
 */
+resource "aws_iam_role" "test" {
+  name               = "test-role"
+  assume_role_policy = "${file("assume-role-policy.json")}"
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "test-policy"
+  description = "A test policy"
+  policy      = "${file("policy-s3-bucket.json")}"
+}
+
+resource "aws_iam_policy_attachment" "test-attach" {
+  name       = "test-attachment"
+  roles      = ["${aws_iam_role.test.name}"]
+  policy_arn = "${aws_iam_policy.policy.arn}"
+}
+
+resource "aws_iam_instance_profile" "test_profile" {
+  name  = "test_profile"
+  roles = ["${aws_iam_role.test.name}"]
+}
 
 # Launch CONFIGURATION FOR AUTOSCALING 
 
